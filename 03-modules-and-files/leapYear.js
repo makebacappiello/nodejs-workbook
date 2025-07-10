59124a4517ab16df5dc46943e18dcc578d66f581
makebacappiello <makebacappiello@gmail.com>
2025-07-10 17:27:31 -0400

    edited page

--- 03-modules-and-files/leapYear.js
+++ 03-modules-and-files/leapYear.js
@@ -27,27 +27,3 @@ if (leapYear === true) {
 } else {
   console.log(`${year} is not a leap year!`);
 }
-
-//function getLeapYear() {
-//   //we are going to use the readFile method from the data.json file
-//   //this method takes in 3 parameters:
-//   //1. - the file we want to read
-//   //2 -the way the file is encoded
-//   //3 - the function we run once we've read the file
-//   //fs. for older module
-//   readFile("./data.json", "utf8", (err, data) => {
-//     console.log(data);
-//   });
-// }
-// getLeapYear();
-//     const birthstoneData = JSON.parse(data);
-//     // tell the user their birthstone
-//     if (birthstoneData[month]) {
-//       console.log(`The birthstone for ${month} is ${birthstoneData[month]}`);
-//     } else {
-//       console.log("Invalid month. Please enter a valid month!");
-//     }
-//   });
-// }
-// //run my function
-// getBirthstone();
