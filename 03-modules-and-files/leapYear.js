4b482883f353295964f76023c1cd964d91e02011
makebacappiello <makebacappiello@gmail.com>
2025-07-10 17:23:42 -0400

    added code for leap year via motions

--- 03-modules-and-files/leapYear.js
+++ 03-modules-and-files/leapYear.js
@@ -14,21 +14,35 @@ import moment from "moment";
 
 moment().isLeapYear();
 
-let leapYear = moment([1978]).isLeapYear(); // true
+let year = Number(process.argv[2]);
+
+let leapYear = moment([year]).isLeapYear(); // true
 console.log(leapYear);
 moment([2001]).isLeapYear(); // false
 moment([2100]).isLeapYear(); // false
 
-const year = process.argv[2];
-
 console.log(year);
-//function getBirthstone() {
+if (leapYear === true) {
+  console.log(`${year} is a leap year!`);
+} else {
+  console.log(`${year} is not a leap year!`);
+}
 
-// readFile("./data.json", "utf8", (err, data) => {
-// console.log(data);
-// const birthstoneData = JSON.parse(data);
-// tell the user their birthstone
-// if (birthstoneData[month]) {
+//function getLeapYear() {
+//   //we are going to use the readFile method from the data.json file
+//   //this method takes in 3 parameters:
+//   //1. - the file we want to read
+//   //2 -the way the file is encoded
+//   //3 - the function we run once we've read the file
+//   //fs. for older module
+//   readFile("./data.json", "utf8", (err, data) => {
+//     console.log(data);
+//   });
+// }
+// getLeapYear();
+//     const birthstoneData = JSON.parse(data);
+//     // tell the user their birthstone
+//     if (birthstoneData[month]) {
 //       console.log(`The birthstone for ${month} is ${birthstoneData[month]}`);
 //     } else {
 //       console.log("Invalid month. Please enter a valid month!");
