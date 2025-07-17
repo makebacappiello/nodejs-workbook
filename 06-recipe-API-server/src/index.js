59fca035d780b06017932c0e616e4c3cb335231e
makebacappiello <makebacappiello@gmail.com>
2025-07-17 11:37:20 -0400

    cleaned and commented code

--- 06-recipe-API-server/src/index.js
+++ 06-recipe-API-server/src/index.js
@@ -1,41 +1,24 @@
-// Write your Recipe CRUD App code here!
-
-// ---------------------------------
-// Boilerplate Code to Set Up Server
-// ---------------------------------
 // ----------------------
 // BOILERPLATE CODE TO SET UP THE SERVER
 // ----------------------
 
-// importing our Node modules
-import express from "express"; // the framework we use to build a web server
-import fs from "fs/promises"; // the File System module that lets us read files
-import { readFile, writeFile } from "fs/promises";
+// importing our Express modules to build a web server easily
+import express from "express";
+
+//Imports file system functions that return Promises — so that I can await reading and writing to files like data.json
+import fs from "fs/promises";
 
 // Creating an instance of the express module so that we can use all of its superpowers, including its functions, properties, etc.
+// I will use it to define my routes
 const app = express();
 
-// Define which port our server should listen to receive requests
+// Defines which port our server should listen to receive requests. I'll access your app by going to http://localhost:3000
 const port = 3000;
 
-// say that we're using JSON data type
-// Our server will receive data as JSON, and respond with JSON
+// The code below tells Express to automatically parse incoming JSON in requests (e.g., from a POST body). Our server will receive data as JSON, and respond with JSON
 app.use(express.json());
 
-// run the function that turns on the server to listen for requests on the port
-app.listen(port, () => {
-  console.log(`My server is listening on port ${port}!`);
-});
-
-app.get("/", (req, res) => {
-  res.send("Hello World of Makeba!");
-});
-
-// say that we're using JSON data type
-// Our server will receive data as JSON, and respond with JSON
-app.use(express.json());
-
-// run the function that turns on the server to listen for requests on the port
+// The function below starts the server and logs a message to let me know it’s running
 app.listen(port, () => {
   console.log(`My server is listening on port ${port}!`);
 });
@@ -43,6 +26,10 @@ app.listen(port, () => {
 // ---------------------------------
 // Helper Functions
 // ---------------------------------
+
+// This function reads my data.json file from the parent folder
+//Then it converts the JSON text into a real JavaScript object/array
+//Then it returns it.
 async function getAllRecipies() {
   const data = await fs.readFile("../data.json", "utf8");
   return JSON.parse(data);
@@ -51,7 +38,7 @@ async function getAllRecipies() {
 // ---------------------------------
 // GET-ONE-RECIPIE
 //----------------------------------
-
+//Similar to getAllRecipies, but only returns one recipe based on the index passed in.
 async function getOneRecipie(recipieIndex) {
   const data = await fs.readFile("../data.json", "utf8");
   const parsedData = JSON.parse(data);
@@ -63,32 +50,33 @@ async function getOneRecipie(recipieIndex) {
 // API Endpoints
 // ---------------------------------
 
-// TODO: API Endpoint for handling GET requests to /get-all-recipes
+// This is the API Endpoint for handling GET requests to /get-all-recipes
+// When the user visits /get-all-recipies, it:
+1;
+//Calls getAllRecipies().
+//2.Logs the recipes to your terminal.
 
 app.get("/get-all-recipies", async (req, res) => {
   const recipies = await getAllRecipies();
   console.log("THESE ARE THE RECIPIES");
 
-  // send the recipies data as JSON in the response
+  // 3. Sends the recipies data as JSON in the response
   //res.send(JSON.stringify(recipies));
   res.json(recipies);
 });
 
-// TODO: API Endpoint for handling GET requests to /get-one-recipe/:index
-
+// This is the API Endpoint for handling GET requests to /get-one-recipe/:index
+//:index is a route parameter, which comes from the URL
 app.get("/get-one-recipie/:index", async (req, res) => {
-  // we got the user's info
+  // Here is where we get the user's info that is defined as a variable
   const recipieIndex = req.params.index;
 
-  // we need to get the recipie from the data.json
+  // We use this constant to define that the program must wait to get the recipie from the data.json
   const recipie = await getOneRecipie(recipieIndex);
   console.log(recipie, `THESE IS THE RECIPIE FROM ${recipieIndex}`);
 
   // then send the recipie back in the response
+
   //res.send(JSON.stringify(recipie));
   res.json(recipie);
 });
-
-// TODO: API Endpoint for handling GET requests to /delete-one-recipe/:index
-
-// TODO: API Endpoint for handling GET requests to /update-one-recipe-name/:index/:newName
